2159f824c181d7aad74580dddc86f2c47fa91fb7
Khush Shah <khush.shah@hertshtengroup.com>
2025-10-28 13:02:19 +0530

    Add echarts-for-react/src/App.tsx

---
+++ echarts-for-react/src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import Echarts from "./components/EChartsExample";
+import LineChart from "./components/LineChart";
+import TimeSeriesChart from "./components/TimeSeriesChart";
+
+function App() {
+  return (
+    <>
+      <div className="w-full min-h-screen flex flex-col items-center gap-6 p-6">
+        <div className="">
+          <h1 className="text-6xl font-bold p-6 bg-amber-600 m-6">You are a legend if you are reading this.</h1>
+        </div>
+        
+        <div className="w-5/6 grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <div className="bg-pink-200 p-6 rounded-lg">
+            <h2 className="text-2xl font-semibold mb-4">Bar Chart</h2>
+            <Echarts />
+          </div>
+          
+          <div className="bg-blue-200 p-6 rounded-lg">
+            <h2 className="text-2xl font-semibold mb-4">Line Chart</h2>
+            <LineChart />
+          </div>
+          
+          <div className="bg-green-200 p-6 rounded-lg lg:col-span-2">
+            <h2 className="text-2xl font-semibold mb-4">Time Series Multi-line Chart</h2>
+            <TimeSeriesChart />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
